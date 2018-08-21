d4e9d9e3e990d58a3c1845b442f4499c3ba579bb
Fernanda Fueltala <desarrollo@micamello.com.ec>
2018-08-21 10:52:50 -0500

    cambios en htaccess y quitar hreflan en cabecera

--- frontend/vista/html/cabecera.php
+++ frontend/vista/html/cabecera.php
@@ -10,7 +10,6 @@
     <meta name="keywords" content="ofertas de trabajo, trabajos, empleos, bolsa de empleos, buscar trabajo, busco empleo, portal de empleo, ofertas de empleo, bolsa de empleo, trabajos en ecuador, paginas de empleo, empleos ecuador, camello">
     <title>MiCamello - Portal de Empleos en Ecuador</title>
     <meta name="description" content="Cientos de empresas publican las mejores ofertas en la bolsa de trabajo Mi Camello Ecuador. Busca empleo y apúntate y sé el primero en postular">
-    <link rel="alternate" hreflang="ec-EC" href="https://www.micamello.com.ec/" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:image" content="https://www.micamello.com.ec/" />
     <link rel="icon" type="image/x-icon" href="<?php echo PUERTO."://".HOST;?>/imagenes/favicon.ico">
