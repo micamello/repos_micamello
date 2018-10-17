d6f8c5f46f77651b9cb8c64de328a8150335a753
micamello <42096699+micamello@users.noreply.github.com>
2018-10-17 10:04:58 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'constantes.php';
 require_once 'init.php';
+include 'multisitios.php';
 
 /*if( (!isset($_GET['mostrar']) || $_GET['mostrar'] != 'logout') && !Model_Usuario::isLoggedIn() && 
     (!isset($_GET['mostrar']))){
@@ -30,11 +31,49 @@ function obtieneControlador($nombre){
     case 'login':
       return 'Login';
     break;
+    case 'contrasena':
+      return 'Contrasena';
+    break;
     case 'logout':
       return 'Logout';
     break;
     case 'perfil':
       return 'Perfil';
+    break;
+    case 'cuestionario':
+      return 'Cuestionario';
+    case 'registro':
+      return 'Registro';
+    break;
+    case 'velocimetro':
+      return 'Velocimetro';
+    break;
+    case 'publicar':
+      return 'Publicar';
+    break;
+    case 'plan':
+      return 'Plan';
+    break;
+    case 'oferta':
+      return 'Oferta';
+    break;
+    case 'postulacion':
+      return 'Postulacion';
+    break;
+    case 'recomendacion':
+      return 'Recomendacion';
+    break;
+    case 'vacantes':
+      return 'Vacantes';
+    break;
+    case 'aspirante':
+      return 'Aspirante';
+    break;
+    case 'informePDF':
+      return 'InformePDF';
+    break;
+    case 'notificacion':
+      return 'Notificacion';
     break;
     default:
       return 'Inicio'; 
--- init.php
+++ init.php
@@ -1,9 +1,15 @@
 <?php
-require_once RUTA_INCLUDES.'Database.php';
-require_once RUTA_INCLUDES.'Utils.php';
+require_once RUTA_INCLUDES.'/phpMailer/PHPMailerAutoload.php';
 
 function cargarClases($nombreClase) {
-  $nombre_archivo = RUTA_FRONTEND . '/'. str_replace('_', '/', $nombreClase) . '.php';
+  $nombre_archivo = RUTA_FRONTEND . str_replace('_', '/', $nombreClase) . '.php';
+  if (file_exists($nombre_archivo)) {
+    include_once( $nombre_archivo );
+  }
+}
+
+function cargarClasesLib($nombreClase) {
+  $nombre_archivo = RUTA_INCLUDES . str_replace('_', '/', $nombreClase) . '.php';
   if (file_exists($nombre_archivo)) {
     include_once( $nombre_archivo );
   }
@@ -11,11 +17,18 @@ function cargarClases($nombreClase) {
 
 spl_autoload_register(null, false);
 spl_autoload_register('cargarClases', false);
+spl_autoload_register('cargarClasesLib', false);
 
 $GLOBALS['db'] = new Database( DBSERVIDOR, DBUSUARIO, DBCLAVE, DBNOMBRE);
 $GLOBALS['db']->connect();
 
-$_SUBMIT = array_merge($_POST, $_GET);
+if(count($_POST) != 0 && $_GET["mostrar"] != "publicar"){ $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING); }
+if(count($_GET) != 0){ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING); }
+if(count($_COOKIE) != 0){ $_COOKIE = filter_input_array(INPUT_COOKIE, FILTER_SANITIZE_STRING); }
 
+//if(count($_SERVER) != 0){ $_SERVER = filter_input_array(INPUT_SERVER, FILTER_SANITIZE_STRING); }
+
+$_SUBMIT = array_merge($_POST, $_GET);
+  
 Utils::createSession(); 
 ?>
