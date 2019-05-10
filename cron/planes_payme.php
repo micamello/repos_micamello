8320c6861901d1648368250f30787d03de27bd63
Fernanda Fueltala <desarrollo@micamello.com.ec>
2019-05-10 09:27:35 -0500

    planes payme

---
+++ cron/planes_payme.php
@@ -0,0 +1,83 @@
+<?php
+ignore_user_abort( true );
+ini_set("max_execution_time", "0");
+ini_set("max_input_time", "0");
+ini_set('memory_limit', "768M");
+set_time_limit(0);
+/*Script que permite la generacion de planes de los usuarios que cancelaron por medio de payme*/
+
+require_once '../constantes.php';
+require_once '../init.php';
+
+//pregunta si ya se esta ejecutando el cron sino crea el archivo
+$resultado = file_exists(CRON_RUTA.'procesando_payme.txt');
+if ($resultado){
+  exit;
+}
+else{
+  Utils::crearArchivo(CRON_RUTA,'procesando_payme.txt','');
+}
+
+$registros = Modelo_Payme::obtieneNoProcesados();
+
+if (!empty($registros) && is_array($registros)){
+  foreach($registros as $registro){  
+    if (empty($registro)){      
+      Utils::envioCorreo('desarrollo@micamello.com.ec','Cron planes_pame.php','Registro no tiene valores '.print_r($registro,true));
+      continue;
+    }
+
+    $cliente = obtenerDatosCliente($registro["custom"]); 
+    if (empty($cliente)){       
+    	Utils::envioCorreo('desarrollo@micamello.com.ec','Cron planes_payme.php','Campo custom no tiene valores '.print_r($registro,true));
+      continue;
+    }    
+    if (empty($registro["txn_id"]) || empty($registro["payment_gross"]) || empty($registro["id_paypal"])){      
+      Utils::envioCorreo('desarrollo@micamello.com.ec','Cron planes_paypal.php','Valores nulos '.print_r($registro,true));
+      continue;
+    }
+     
+    switch($registro["payment_status"]){
+      //si realizo el pago
+      case "Completed":
+         $procesador = (object) array('id'=>$registro["id_paypal"],
+                                      'tipo'=>'paypal',
+                                      'trans'=>$registro["txn_id"],
+                                      'monto'=>$registro["payment_gross"]);
+         $objSubscripcion = new Proceso_Subscripcion($cliente,$cliente->plan,$procesador);
+         $objSubscripcion->procesar();        
+      break;
+      //si cancelo el pago
+      case "Reversed":
+         $procesador = (object) array('id'=>$registro["id_paypal"],
+                                      'tipo'=>'paypal',
+                                      'trans'=>$registro["parent_txn_id"],
+                                      'monto'=>$registro["payment_gross"]);
+         $objCancelacion = new Proceso_Cancelacion($cliente,$cliente->plan,$procesador);
+         $objCancelacion->procesar();
+      break;
+    }     
+  }
+}
+
+//elimina archivo de procesamiento
+unlink(CRON_RUTA.'procesando_paypal.txt');
+
+function obtenerDatosCliente($custom){
+  if (empty($custom)){ return false; }
+  $datos = explode('|',$custom);
+  if (!is_array($datos)){ return false; }  
+  if (count($datos)<6){ return false; }
+  $usuario = array('plan'=>$datos[0],
+                   'id'=>$datos[1], 
+                   'tipo'=>$datos[2],
+                   'nombres'=>Utils::no_carac($datos[3]),  
+                   'correo'=>$datos[4], 
+                   'tipodoc'=>$datos[5], 
+                   'telefono'=>$datos[6], 
+                   'dni'=>$datos[7], 
+                   'direccion'=>Utils::no_carac($datos[8]));
+  $obj = (object) $usuario;  
+  return $obj;
+}
+?>
--- paginarespuesta.php
+++ paginarespuesta.php
@@ -2,12 +2,8 @@
 require_once 'constantes.php';
 require_once 'init.php';
 
-//Misma clave que se usa para el envio a VPOS2
-//$claveSecreta = 'mjNkPqNvrjUxZAH.97676492';
-
 //purchaseVerication que devuelve la Pasarela de Pagos
 $purchaseVericationVPOS2 = $_POST['purchaseVerification'];
-//echo $purchaseVericationVPOS2 . "\n";
 //purchaseVerication que genera el comercio
 $purchaseVericationComercio = openssl_digest(PAYME_ACQUIRERID . 
                                              PAYME_IDCOMMERCE . 
@@ -16,10 +12,10 @@ $purchaseVericationComercio = openssl_digest(PAYME_ACQUIRERID .
                                              PAYME_CURRENCY_CODE . 
                                              $_POST['authorizationResult'] . 
                                              PAYME_SECRET_KEY, 'sha512');
-//echo $purchaseVericationComercio;
 try{
   if ($purchaseVericationVPOS2 == $purchaseVericationComercio || $purchaseVericationVPOS2 == "") {
     $vl_insert = array();
+    $vl_insert["fecha"] = date("Y-m-d H:i:s");
     $vl_insert["authorizationResult"] = $_POST["authorizationResult"];
     $vl_insert["authorizationCode"] = $_POST["authorizationCode"];
     $vl_insert["errorCode"] = $_POST["errorCode"];
@@ -39,41 +35,26 @@ try{
     $vl_insert["reserved15"] = $_POST["reserved15"];
     $vl_insert["reserved16"] = $_POST["reserved16"];
     $vl_insert["reserved17"] = $_POST["reserved17"];
+    $vl_insert["reserved18"] = $_POST["reserved18"];
+    $vl_insert["reserved19"] = $_POST["reserved19"];
+    $vl_insert["purchaseOperationNumber"] = $_POST["purchaseOperationNumber"];
+    $vl_insert["shippingPhone"] = $_POST["shippingPhone"];
+    $vl_insert["shippingAddress"] = $_POST["shippingAddress"];
+    $vl_insert["descriptionProducts"] = $_POST["descriptionProducts"];
+    $vl_insert["shippingEmail"] = $_POST["shippingEmail"];
+    $vl_insert["shippingZIP"] = $_POST["shippingZIP"];
+    $vl_insert["purchaseAmount"] = $_POST["purchaseAmount"];
+    $vl_insert["IDTransaction"] = $_POST["IDTransaction"];
     Utils::envioCorreo("desarrollo@micamello.com.ec","CRON PAYME",print_r($_POST,true));    
-    //if (!Modelo_Paypal::guardar($vl_insert)){
-    //  throw new Exception("Error Insert IPN Paypal");
-    //} 
+    if (!Modelo_Payme::guardar($vl_insert)){
+      throw new Exception("Error Insert IPN Payme");
+    } 
   }
+  Utils::doRedirect(PUERTO.'://'.HOST.'/compraplan/exito/');
 }
 catch(Exception $e){    
   Utils::envioCorreo('desarrollo@micamello.com.ec',$e->getMessage(),print_r($_POST,true));
+  Utils::doRedirect(PUERTO.'://'.HOST.'/compraplan/error/');
 }
 header("HTTP/1.1 200 OK");    
-
-print_r($_POST);
-
-//Si ambos datos son iguales
-if ($purchaseVericationVPOS2 == $purchaseVericationComercio || $purchaseVericationVPOS2 == "") {
-?>    	
-  <table>
-    <tr><td>AuthorizationResult</td><td><?php echo $_POST['authorizationResult'];?></td></tr>
-    <tr><td>AuthorizationCode</td><td><?php echo $_POST['authorizationCode'];?></td></tr>
-    <tr><td>ErrorCode</td><td><?php echo $_POST['errorCode'];?></td></tr>
-    <tr><td>ErroMessage</td><td><?php echo $_POST['errorMessage'];?></td></tr>
-    <tr><td>Bin</td><td><?php echo $_POST['bin'];?></td></tr>
-    <tr><td>Brand</td><td><?php echo $_POST['brand'];?></td></tr>
-    <tr><td>PaymentReferenceCode</td><td><?php echo $_POST['paymentReferenceCode'];?></td></tr>
-    <!--Ejemplo recepción de campos reservados en parametro reserved1-->
-    <tr><td>Reserved1</td><td><?php echo $_POST['reserved1'];?></td></tr>
-    <tr><td>Reserved22</td><td><?php echo $_POST['reserved22'];?></td></tr>
-    <tr><td>Reserved23</td><td><?php echo $_POST['reserved23'];?></td></tr>
-    <tr><td>Número de Operacion</td><td><?php echo $_POST['purchaseOperationNumber'];?></td></tr>
-    <tr><td>Monto</td><td><?php echo "S/. " . $_POST['purchaseAmount'];?></td></tr>
-</table>
-    
-<?php
-    //Si ambos datos son diferentes
-} else {
-    echo "<h1>Transacción Invalida. Los datos fueron alterados en el proceso de respuesta.</h1>";
-}   
-?> 
+?>
