1bc542ae9b7acd6ec4d93a6d54570d38a5d57488
Virgilio Pozo <administrador.gye@micamello.com.ec>
2019-07-18 15:46:09 -0500

    cambios EN VP

--- cron/alerta_ofertas.php
+++ cron/alerta_ofertas.php
@@ -20,40 +20,72 @@ else{
 }
 
 $result_set = Modelo_Usuario::obtieneTodosCandidatos();
+$fechaInicial = new DateTime("now");
+
+$correosEnviados = 0;
+$id_ultimoUsuario = 0;
 while( $rows = mysqli_fetch_array( $result_set, Database::ASSOC) ){
-	$mail_ofertas = ""; 
-	$arr_areas = Modelo_UsuarioxArea::consultarSubareas($rows["id_usuario"]);
-  $str_areas = implode($arr_areas,',');
-  if (empty($arr_areas)){
-  	continue;
-  }
-  echo "<br>usuario: ".$rows["id_usuario"]." / ".$rows["nombres"]." / ".$rows["apellidos"]."<br>";
-  echo "areas: ".$str_areas."<br>";  
-	$ofertas = Modelo_Oferta::ofertasDiarias($rows["id_pais"],$str_areas);
-  if (empty($ofertas)){
-  	continue;
-  }
+  echo "correos enviados: ".$correosEnviados."<br>Id_usuario:".$rows['id_usuario']." <br>";
+  $fechaFin = new DateTime("now");
+  $diferencia = $fechaInicial->diff($fechaFin)->format("%H:%i:%s");
+  $diferencia = explode(':', $diferencia);
+  $minutosDiff = ($diferencia[0]*60) + $diferencia[1] + ($diferencia[2]/60);
 
-  $sucursal = Modelo_Sucursal::consultaxPais($rows["id_pais"]);
+	if($correosEnviados >= 5 || $minutosDiff >= 60){
+    if($ultimousuariofile = file_exists(CRON_RUTA.'ultimousuario.txt')){
+      unlink(CRON_RUTA.'ultimousuario.txt');
+    }
+    Utils::crearArchivo(CRON_RUTA, 'ultimousuario.txt', $rows["id_usuario"]);
+    break;
+  }else{
+    $id_ultimoUsuario = $rows["id_usuario"];
+    if($ultimousuariofile = file_exists(CRON_RUTA.'ultimousuario.txt')){
+      $file = fopen("ultimousuario.txt", "r");
+      $contenido = fgets($file);
+      echo "ultimo usuario row: ".$id_ultimoUsuario." ----- ".$contenido."<br>";
+      if((int)$id_ultimoUsuario != (int)$contenido){
+        continue;
+      }
+      fclose($file);
+      unlink(CRON_RUTA.'ultimousuario.txt');
+    }
 
-  foreach($ofertas as $oferta){    
-    $mail_ofertas .= utf8_encode($oferta["titulo"])."<br>";
-    $nombre_empresa = ($oferta["confidencial"]) ? "Nombre de la Empresa - Confidencial" : utf8_encode($oferta["empresa"]);
-    $mail_ofertas .= $nombre_empresa." - ".utf8_encode($oferta["provincia"])." / ".utf8_encode($oferta["ciudad"])."<br>";
-    $mail_ofertas .= "<a href='".PUERTO."://".$sucursal["dominio"]."/detalleOferta/oferta/".Utils::encriptar($oferta["id_ofertas"])."/'>Ver Oferta</a><br><br>";
-    echo $mail_ofertas."<br>";
-  }
-  //envio de correo al candidato
-  if (!empty($mail_ofertas)){
-    $nombre_mostrar = ucfirst(utf8_encode($rows["nombres"]))." ".ucfirst(utf8_encode($rows["apellidos"]));
-    $email_body = Modelo_TemplateEmail::obtieneHTML("OFERTAS_LABORALES");
-    $email_body = str_replace("%NOMBRES%", $nombre_mostrar, $email_body);   
-    $email_body = str_replace("%OFERTAS%", $mail_ofertas, $email_body);
-    //echo $email_body;   
-    Utils::envioCorreo($rows["correo"],"Ofertas Laborales",$email_body);     
+    $mail_ofertas = ""; 
+    $arr_areas = Modelo_UsuarioxArea::consultarSubareas($rows["id_usuario"]);
+    $str_areas = implode($arr_areas,',');
+    if (empty($arr_areas)){
+      continue;
+    }
+    echo "<br>usuario: ".$rows["id_usuario"]." / ".$rows["nombres"]." / ".$rows["apellidos"]."<br>";
+    echo "areas: ".$str_areas."<br>";  
+    $ofertas = Modelo_Oferta::ofertasDiarias($rows["id_pais"],$str_areas);
+    if (empty($ofertas)){
+      continue;
+    }
+
+    $sucursal = Modelo_Sucursal::consultaxPais($rows["id_pais"]);
+
+    foreach($ofertas as $oferta){    
+      $mail_ofertas .= utf8_encode($oferta["titulo"])."<br>";
+      $nombre_empresa = ($oferta["confidencial"]) ? "Nombre de la Empresa - Confidencial" : utf8_encode($oferta["empresa"]);
+      $mail_ofertas .= $nombre_empresa." - ".utf8_encode($oferta["provincia"])." / ".utf8_encode($oferta["ciudad"])."<br>";
+      $mail_ofertas .= "<a href='".PUERTO."://".$sucursal["dominio"]."/detalleOferta/oferta/".Utils::encriptar($oferta["id_ofertas"])."/'>Ver Oferta</a><br><br>";
+      echo $mail_ofertas."<br>";
+    }
+    //envio de correo al candidato
+    if (!empty($mail_ofertas)){
+      $nombre_mostrar = ucfirst(utf8_encode($rows["nombres"]))." ".ucfirst(utf8_encode($rows["apellidos"]));
+      $email_body = Modelo_TemplateEmail::obtieneHTML("OFERTAS_LABORALES");
+      $email_body = str_replace("%NOMBRES%", $nombre_mostrar, $email_body);   
+      $email_body = str_replace("%OFERTAS%", $mail_ofertas, $email_body);
+      // Utils::envioCorreo($rows["correo"],"Ofertas Laborales",$email_body); 
+      Utils::envioCorreo("edervpozo@gmail.com","Ofertas Laborales",$email_body);
+      $correosEnviados++;
+      echo "<br><br><br>";  
+    }
   }
 }
-
 //elimina archivo de procesamiento
+// unlink(CRON_RUTA."ultimousuario.txt");
 unlink(CRON_RUTA.'alerta_ofertas.txt');
 ?>
