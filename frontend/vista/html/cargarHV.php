82b9a99fbdb05f7c0ba65cb40a35c50e25f0570e
Mariana Vera <desarrollo2@micamello.com.ec>
2019-09-15 13:50:53 -0500

    cambios en hoja vida MV

--- frontend/vista/html/cargarHV.php
+++ frontend/vista/html/cargarHV.php
@@ -1,3 +1,6 @@
+<?php require_once ('includes/Mobile-Detect/Mobile_Detect.php'); 
+      $detect = new Mobile_Detect(); ?>
+
 <form action="<?php echo PUERTO."://".HOST."/subirHv/"; ?>" method='POST' id='cargarHVForm' enctype="multipart/form-data">
   <div class="text-center">
   <h2 class="titulo">Cargar Hoja de Vida</h2>
@@ -6,17 +9,32 @@
     <div class="row">
       <div class="col-md-6 col-sm-12 col-md-offset-3">
         
+        <?php if ($detect->isMobile()==false) { ?>
+        <!--Si es web-->
         <div id="carga" style="margin: 50px 0px;" class="recuadro-perfil panel panel-default"><br>
           <img width="100%" alt="fotoPerfil" src="<?php echo PUERTO.'://'.HOST.'/imagenes/cv.png' ?>" style="border-radius: 20px 20px 0px 0px;">
           <div class="perfil-cuadro" id="err_img" align="center">
-            <label style="font-size: 16pt;" class="text-center" id="cargarHV">Seleccione archivo<br></label><br>
-            <small style="font-size: 100%">(.PDF, .doc, .docx/máx: 2mb)</small>
+            <label style="font-size: 16pt;" class="text-center" id="cargarHV">Seleccione archivo<br></label>
+            <br>
+            <small style="font-size: 100%">(.PDF, .doc, .docx/máx: 5mb)</small>
           </div>
         </div>
+         <input type="file" name="userHV" id="userHV" style="display: none;" class="cargahv" accept=".pdf,.docx,.doc">
+         <!-- <a href="#" style="font-size: 20pt;" class="btn-light-blue" disabled="">GUARDAR</a> -->
+        <?php }?>
 
-        <input type="file" name="userHV" id="userHV" accept=".pdf,.docx,.doc">
-        <!-- <a href="#" style="font-size: 20pt;" class="btn-light-blue" disabled="">GUARDAR</a> -->
+        
+        <?php if ($detect->isMobile()==true) { ?>
+        <!--Si es movil-->
+            <input type="file" name="userHV" id="userHV" class="in-btn" accept=".pdf,.docx,.doc">
+            <span class="new">SELECCIONE ARCHIVO</span>
+            <br><br><br><br>
+        <?php }?>
+
+
         <input type="submit" name="hvButton" id="hvButton" value="Guardar" class="btn-light-blue" style="font-size: 20pt;">
+
+
       </div>
     </div>
   </div>
